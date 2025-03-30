9940608939dec7136030c9b34706a701576917f8
PEZHMAN-KM <kazemimir.pezhman@gmail.com>
2025-03-30 04:31:32 +0330

    test commit (nothing change)

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -30,7 +30,6 @@ const ArrowIcon = ({ className, ...props }) => {
 }
 
 // minus
-
 {
   /* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
