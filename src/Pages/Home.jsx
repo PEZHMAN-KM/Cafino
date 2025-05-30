bae17b43fd307deb61e20817c980b6291c957c5e
Revenge KM <Kazemimir.pezhman@gmail.com>
2025-05-31 02:51:31 +0330

    add order for item page and like

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Header from "../Componnets/Header.jsx";
 import Footer from "../Componnets/Footer.jsx";
@@ -10,6 +11,38 @@ import bannerImage from "../../public/Banner.jpg";
 import { BASE_PATH } from "../constants/paths.js";
 import axios from "axios";
 
+const Plus = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <g strokeWidth={0} />
+    <g strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M5 12h14m-7-7v14"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const Minus = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none">
+    <g strokeWidth={0} />
+    <g strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      fillRule="evenodd"
+      d="M18 10a1 1 0 0 1-1 1H3a1 1 0 1 1 0-2h14a1 1 0 0 1 1 1"
+    />
+  </svg>
+);
+
 const formatPrice = (num) => {
   if (num == null || isNaN(num)) return "";
   return Number(num).toLocaleString("en-US");
@@ -18,10 +51,42 @@ const formatPrice = (num) => {
 function Home() {
   const [hideIcons, setHideIcons] = useState(false);
   const scrollContainerRef = useRef(null);
+  const navigate = useNavigate();
 
   const [selectedCategory, setSelectedCategory] = useState(1);
   const [items, setItems] = useState([]);
   const [error, setError] = useState(null);
+
+  const [orderCounts, setOrderCounts] = useState({});
+
+  const [newlyAddedId, setNewlyAddedId] = useState(null);
+  const [removingId, setRemovingId] = useState(null);
+
+  const updateOrder = (id, newCount) => {
+    const updatedCounts = { ...orderCounts };
+
+    if (newCount > 0) {
+      updatedCounts[id] = newCount;
+    } else {
+      delete updatedCounts[id];
+    }
+
+    const orderArray = Object.entries(updatedCounts).map(([key, value]) => [
+      Number(key),
+      value,
+    ]);
+    localStorage.setItem("order", JSON.stringify(orderArray));
+    setOrderCounts(updatedCounts);
+  };
+
+  useEffect(() => {
+    const storedOrder = JSON.parse(localStorage.getItem("order") || "[]");
+    const counts = {};
+    storedOrder.forEach(([id, count]) => {
+      counts[id] = count;
+    });
+    setOrderCounts(counts);
+  }, []);
 
   useEffect(() => {
     const scrollContainer = scrollContainerRef.current;
@@ -97,15 +162,42 @@ function Home() {
     fetchItems();
   }, [selectedCategory]);
 
-  const largeHeight = 176; // معادل h-44 در Tailwind (44 * 4 = 176px)
-  const smallHeight = 68; // معادل h-17 در Tailwind (17 * 4 = 68px)
+  const increaseCount = (id) => {
+    const newCount = (orderCounts[id] || 0) + 1;
+    updateOrder(id, newCount);
+  };
+
+  const decreaseCount = (id) => {
+    const current = orderCounts[id] || 1;
+
+    if (current === 1) {
+      setRemovingId(id);
+      setTimeout(() => {
+        updateOrder(id, 0);
+        setRemovingId(null);
+      }, 400);
+    } else {
+      updateOrder(id, current - 1);
+    }
+  };
+
+  const handleAddToOrder = (id) => {
+    updateOrder(id, 1);
+    setNewlyAddedId(id);
+    setTimeout(() => setNewlyAddedId(null), 400);
+  };
+
+  function selectFood(food_id) {
+    localStorage.setItem("show_food", food_id);
+    navigate("/item");
+  }
 
   return (
     <>
       <div
         ref={scrollContainerRef}
         className={`bg-backgroundcolor dark:bg-backgroundcolorDark w-screen h-screen overflow-y-auto scrollbar scrollbar-none overflow-x-hidden pb-26 md:pb-5 transition-colors duration-300 ${
-          !hideIcons ? "pb-60 md:pb-49" : "pb-26 md:pb-5"
+          !hideIcons ? "pb-60 md:pb-5" : "pb-26 md:pb-49"
         }`}>
         <Header page={1} />
         <SubHeder
@@ -130,28 +222,86 @@ function Home() {
         {!error && (
           <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-9 gap-y-4 gap-x-2 mt-2 justify-center items-center">
             {items.map((item) => (
-              <a href="/item" key={item.id}>
+              <div
+                onClick={() => selectFood(item.id)}
+                href="/item"
+                key={item.id}>
                 <div
                   className={`${
                     item.in_sale
                       ? "bg-slowprimary dark:bg-slowprimaryDark"
                       : "bg-white dark:bg-darkpalleteDark"
-                  } rounded-3xl w-ful h-fit p-3 m-auto transition-all duration-300 hover:scale-102 hover:bg-highgray`}>
+                  } rounded-3xl w-ful h-fit p-3 m-auto text-start transition-all duration-300 hover:scale-102 hover:bg-highgray`}>
                   <img
                     className="w-full aspect-square object-cover rounded-2xl dark:opacity-90 transition-opacity duration-300"
                     src={item.pic_url ? item.pic_url : itemImage}
                     alt={item.name}
                   />
-                  <h1 className="text-2xl font-bold mt-2 dark:text-white transition-colors duration-300">
-                    {item.name}
-                  </h1>
-                  <h3 className="text-balance mt-1 dark:text-slowgray transition-colors duration-300">
-                    {item.description}
-                  </h3>
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <h1 className="text-2xl font-bold mt-2 dark:text-white transition-colors duration-300">
+                        {item.name}
+                      </h1>
+                      <h3 className="text-balance mt-1 dark:text-slowgray transition-colors duration-300">
+                        {item.description}
+                      </h3>
+                    </div>
+                    {orderCounts[item.id] ? (
+                      <div>
+                        <h1
+                          className={`bg-primary w-6 h-6 rounded-full flex justify-center items-center font-bold text-white ${
+                            newlyAddedId === item.id
+                              ? "animate-scale-up"
+                              : removingId === item.id
+                              ? "animate-scale-out"
+                              : ""
+                          }`}>
+                          {orderCounts[item.id]}
+                        </h1>
+                      </div>
+                    ) : null}
+                  </div>
                   <div className="flex mt-3 h-20 justify-between items-center">
-                    <button className="flex justify-center items-center text-2xl rounded-2xl bg-primary dark:bg-primaryDark hover:bg-primaryDark dark:hover:bg-primary text-white w-13 h-13 transition-colors duration-300">
-                      +
-                    </button>
+                    {orderCounts[item.id] ? (
+                      <div
+                        className={`flex items-center gap-2 transition-all duration-300 ${
+                          newlyAddedId === item.id
+                            ? "animate-scale-up"
+                            : removingId === item.id
+                            ? "animate-scale-out"
+                            : ""
+                        }`}>
+                        <button
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            increaseCount(item.id);
+                          }}
+                          className="w-7 h-7 flex items-center justify-center bg-primary dark:bg-primaryDark rounded-full hover:bg-primaryDark dark:hover:bg-primary transition-colors duration-300">
+                          <Plus className={"w-7 stroke-white"} />
+                        </button>
+                        <button
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            decreaseCount(item.id);
+                          }}
+                          className="w-7 h-7 border-2 border-primary dark:border-primaryDark rounded-full flex items-center justify-center hover:bg-primaryDark dark:hover:bg-primary transition-all duration-300">
+                          <Minus
+                            className={
+                              "w-3 fill-black dark:fill-white hover:fill-white transition-colors duration-300"
+                            }
+                          />
+                        </button>
+                      </div>
+                    ) : (
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleAddToOrder(item.id);
+                        }}
+                        className="flex justify-center items-center rounded-2xl bg-primary dark:bg-primaryDark hover:bg-primaryDark dark:hover:bg-primary w-13 h-13 transition-colors duration-300">
+                        <Plus className={"w-10 stroke-white"} />
+                      </button>
+                    )}
                     <div>
                       {item.sale_price && (
                         <div className="flex justify-center items-end gap-1">
@@ -173,7 +323,7 @@ function Home() {
                     </div>
                   </div>
                 </div>
-              </a>
+              </div>
             ))}
           </div>
         )}
--- src/Pages/Item.jsx
+++ src/Pages/Item.jsx
@@ -1,5 +1,8 @@
-import itemImage from "../../public/Banner.jpg";
+import itemImage from "../../public/No_Item.png";
 import { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import { BASE_PATH } from "../constants/paths";
+import { useNavigate } from "react-router-dom";
 
 const ArrowIcon = ({ className }) => {
   return (
@@ -35,47 +38,94 @@ const Like = ({ className }) => (
   </svg>
 );
 
+const Plus = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <g strokeWidth={0} />
+    <g strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M5 12h14m-7-7v14"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const Minus = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none">
+    <g strokeWidth={0} />
+    <g strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      fillRule="evenodd"
+      d="M18 10a1 1 0 0 1-1 1H3a1 1 0 1 1 0-2h14a1 1 0 0 1 1 1"
+    />
+  </svg>
+);
+
+const formatPrice = (num) => {
+  if (num == null || isNaN(num)) return "";
+  return Number(num).toLocaleString("en-US");
+};
+
 // This component is used to display the size options for the item. (Not used in the current version)
-const Size = () => (
-  <div className="p-2">
-    <h1 className="text-2xl font-bold p-1 dark:text-white transition-colors duration-300">
-      اندازه
-    </h1>
-    <div className="flex justify-between items-center px-2 pt-3">
-      <button className="border-2 border-primary dark:border-primaryDark bg-slowprimary dark:bg-subprimaryDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
-        کوچک
-      </button>
-      <button className="border-2 border-slowgray dark:border-graypalleteDark bg-white dark:bg-darkpalleteDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
-        متوسط
-      </button>
-      <button className="border-2 border-slowgray dark:border-graypalleteDark bg-white dark:bg-darkpalleteDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
-        بزرگ
-      </button>
-    </div>
-  </div>
-);
+// const Size = () => (
+//   <div className="p-2">
+//     <h1 className="text-2xl font-bold p-1 dark:text-white transition-colors duration-300">
+//       اندازه
+//     </h1>
+//     <div className="flex justify-between items-center px-2 pt-3">
+//       <button className="border-2 border-primary dark:border-primaryDark bg-slowprimary dark:bg-subprimaryDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
+//         کوچک
+//       </button>
+//       <button className="border-2 border-slowgray dark:border-graypalleteDark bg-white dark:bg-darkpalleteDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
+//         متوسط
+//       </button>
+//       <button className="border-2 border-slowgray dark:border-graypalleteDark bg-white dark:bg-darkpalleteDark w-30 px-8 py-3 rounded-2xl transition-colors duration-300">
+//         بزرگ
+//       </button>
+//     </div>
+//   </div>
+// );
 
-const NamePanel = ({ className }) => (
+const NamePanel = ({ className, name, category, price, sale_price }) => (
   <div className={className}>
-    <div className="p-2 bg-slowprimary dark:bg-subprimaryDark rounded-2xl border-1 border-primary dark:border-primaryDark transition-colors duration-300">
-      <h1 className="text-2xl font-extrabold p-1 dark:text-white transition-colors duration-300">
-        کافه موکا
-      </h1>
-      <h3 className="text-xl font-bold p-1 pt-0 dark:text-slowgray transition-colors duration-300">
-        گرم
-      </h3>
+    <div className="p-2 flex justify-between items-center bg-slowprimary dark:bg-subprimaryDark rounded-2xl border-1 border-primary dark:border-primaryDark transition-colors duration-300">
+      <div>
+        <h1 className="text-2xl font-extrabold p-1 dark:text-white transition-colors duration-300">
+          {name}
+        </h1>
+        <h3 className="text-xl font-bold p-1 pt-0 dark:text-slowgray transition-colors duration-300">
+          {category}
+        </h3>
+      </div>
+      {sale_price && (
+        <div className="flex flex-col items-start px-2 py-2 w-30 rounded-xl bg-backgroundcolor dark:bg-backgroundcolorDark justify-center gap-1">
+          <h1 className="dark:text-white font-semibold w-full">قیمت اصلی :</h1>
+          <h1 className="text-lg font-medium w-full text-end dark:text-white line-through transition-colors duration-300">
+            {formatPrice(price)} تومان
+          </h1>
+        </div>
+      )}
     </div>
   </div>
 );
 
-const Description = ({ className }) => (
+const Description = ({ className, description }) => (
   <div className={className}>
     <div className="p-2">
       <h1 className="text-2xl font-bold p-1 dark:text-white transition-colors duration-300">
         توضیحات
       </h1>
       <p className="px-3 pt-1 dark:text-slowgray transition-colors duration-300">
-        250 میلی لیتر. یک شات اسپرسو 20% عربیکا و 80% روبوستا. شیر. سیروپ کارامل
+        {description}
       </p>
     </div>
   </div>
@@ -83,6 +133,52 @@ const Description = ({ className }) => (
 
 function Item() {
   const [isLiked, setIsLiked] = useState(false);
+  const id = Number(localStorage.getItem("show_food"));
+  const navigate = useNavigate();
+
+  const [item, setItem] = useState(null);
+  const [orderCount, setOrderCount] = useState(0);
+  const [removing, setRemoving] = useState(false);
+
+  useEffect(() => {
+    const likedItems = JSON.parse(localStorage.getItem("liked_items") || "[]");
+    setIsLiked(likedItems.includes(id));
+  }, [id]);
+
+  const handleLikeClick = () => {
+    let likedItems = JSON.parse(localStorage.getItem("liked_items") || "[]");
+
+    if (likedItems.includes(id)) {
+      likedItems = likedItems.filter((itemId) => itemId !== id);
+      setIsLiked(false);
+    } else {
+      likedItems.push(id);
+      setIsLiked(true);
+    }
+
+    localStorage.setItem("liked_items", JSON.stringify(likedItems));
+  };
+
+  useEffect(() => {
+    async function fetchItem() {
+      try {
+        const response = await axios.post(
+          `${BASE_PATH}/food/get_food_list_by_id`,
+          [id],
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setItem(response.data[0]);
+      } catch (error) {
+        console.error("Error loading item:", error);
+      }
+    }
+
+    if (id) fetchItem();
+  }, [id]);
 
   useEffect(() => {
     if (localStorage.getItem("theme") == "dark") {
@@ -92,28 +188,96 @@ function Item() {
     }
   });
 
-  const handleLikeClick = () => {
-    setIsLiked(!isLiked);
+  useEffect(() => {
+    const storedOrder = localStorage.getItem("order");
+    const orderArray = storedOrder ? JSON.parse(storedOrder) : [];
+    const existingItem = orderArray.find(([itemId]) => itemId === id);
+    if (existingItem) {
+      setOrderCount(existingItem[1]);
+    }
+  }, [id]);
+
+  const updateLocalStorage = (newCount) => {
+    let orderArray = localStorage.getItem("order");
+    orderArray = orderArray ? JSON.parse(orderArray) : [];
+    const index = orderArray.findIndex(([itemId]) => itemId === id);
+
+    if (index !== -1) {
+      if (newCount > 0) {
+        orderArray[index][1] = newCount;
+      } else {
+        orderArray.splice(index, 1);
+      }
+    } else if (newCount > 0) {
+      orderArray.push([id, newCount]);
+    }
+
+    localStorage.setItem("order", JSON.stringify(orderArray));
   };
+
+  const handleAddToOrder = () => {
+    setOrderCount(1);
+    updateLocalStorage(1);
+  };
+
+  const increaseCount = () => {
+    const newCount = orderCount + 1;
+    setOrderCount(newCount);
+    updateLocalStorage(newCount);
+  };
+
+  const decreaseCount = () => {
+    if (orderCount === 1) {
+      setRemoving(true);
+      setTimeout(() => {
+        setOrderCount(0);
+        updateLocalStorage(0);
+        setRemoving(false);
+      }, 300);
+    } else {
+      const newCount = orderCount - 1;
+      setOrderCount(newCount);
+      updateLocalStorage(newCount);
+    }
+  };
+
+  function exit() {
+    localStorage.removeItem("show_food");
+    navigate(-1);
+  }
+
+  if (!item)
+    return (
+      <div className="flex flex-col gap-2 justify-center items-center w-screen h-screen">
+        <h1 className="text-2xl font-bold dark:text-white">آیتم یافت نشد!</h1>
+        <button onClick={() => exit()}>
+          <div className="bg-white dark:bg-darkpalleteDark p-5 rounded-2xl w-full flex items-center text-highgray dark:text-highgrayDark gap-3 hover:bg-slowprimary dark:hover:bg-subprimaryDark hover:text-black dark:hover:text-white transition-all duration-200">
+            <ArrowIcon className="w-8 rotate-180 stroke-3 dark:hover:text-white" />
+            <h1 className="text-2xl font-bold dark:text-white">برگشت</h1>
+          </div>
+        </button>
+      </div>
+    );
+
   return (
     <>
       <div className="lg:flex">
-        <div className="overflow-auto lg:flex-1/2 xl:flex-1/3 lg:flex lg:justify-center lg:items-center bg-backgroundcolor dark:bg-backgroundcolorDark pb-25 lg:pb-0 h-screen overflow-x-hidden transition-colors duration-300">
+        <div className="overflow-auto scrollbar scrollbar-none lg:flex-1/2 xl:flex-1/3 lg:flex lg:justify-center lg:items-center bg-backgroundcolor dark:bg-backgroundcolorDark pb-25 lg:pb-0 h-screen overflow-x-hidden transition-colors duration-300">
           <div className="relative lg:absolute">
             <img
               className="w-screen aspect-square object-cover p-2 rounded-3xl"
-              src={itemImage}
+              src={item.pic_url || itemImage}
               alt=""
             />
-            <div className="flex lg:justify-between p-2 lg:items-center lg:gap-2">
+            <div className="flex lg:justify-between md:p-2 lg:items-center lg:gap-2">
               <div className="absolute lg:static lg:top-auto lg:left-auto lg:w-1/2 lg:pt-0 top-5 right-5">
                 <button
                   onClick={handleLikeClick}
-                  className={`bg-white dark:bg-darkpalleteDark p-5 rounded-2xl lg:w-full flex items-center gap-3 text-highgray dark:text-highgrayDark transition-all duration-200 group
+                  className={`bg-white dark:bg-darkpalleteDark p-5 rounded-2xl lg:w-full flex items-center gap-3 text-highgray dark:text-highgrayDark transition-all duration-200 group hover:bg-slowprimary dark:hover:bg-subprimaryDark hover:text-black dark:hover:text-white
                   ${
                     isLiked
                       ? "bg-slowprimary dark:bg-darkpalleteDark text-red-500"
-                      : "hover:bg-slowprimary dark:hover:bg-subprimaryDark hover:text-black dark:hover:text-white"
+                      : ""
                   }`}>
                   <Like
                     className={`w-8 transition-all duration-200 
@@ -123,27 +287,41 @@ function Item() {
                         : "stroke-highgray dark:stroke-highgrayDark fill-highgray dark:fill-highgrayDark group-hover:fill-black dark:group-hover:fill-white group-hover:stroke-black dark:group-hover:stroke-white"
                     }`}
                   />
-                  <h1 className="hidden lg:block text-2xl font-bold dark:text-white">
+                  <h1
+                    className={`hidden lg:block text-2xl font-bold ${
+                      isLiked
+                        ? "dark:text-red-300 text-red-700"
+                        : "dark:text-white "
+                    }`}>
                     علاقه مندی
                   </h1>
                 </button>
               </div>
               <div className="absolute lg:static lg:top-auto lg:right-auto lg:w-1/2 lg:pt-0 top-5 left-5">
-                <a href="Home">
-                  <div className="bg-white dark:bg-darkpalleteDark p-5 rounded-2xl lg:w-full flex items-center text-highgray dark:text-highgrayDark gap-3 hover:bg-slowprimary dark:hover:bg-subprimaryDark hover:text-black dark:hover:text-white transition-all duration-200">
-                    <ArrowIcon className="w-8 rotate-180 stroke-3  dark:hover:text-white" />
-                    <h1 className="hidden lg:block text-2xl font-bold dark:text-white">
-                      برگشت
-                    </h1>
-                  </div>
-                </a>
+                <button
+                  onClick={() => exit()}
+                  className="bg-white dark:bg-darkpalleteDark p-5 rounded-2xl lg:w-full flex items-center gap-3 text-highgray dark:text-highgrayDark transition-all duration-200 hover:bg-slowprimary dark:hover:bg-subprimaryDark hover:text-black dark:hover:text-white">
+                  <ArrowIcon className="w-8 rotate-180 stroke-3 dark:hover:text-white" />
+                  <h1 className="hidden lg:block text-2xl font-bold dark:text-white">
+                    برگشت
+                  </h1>
+                </button>
               </div>
             </div>
           </div>
           <div>
             <div className="w-screen lg:w-auto p-2 pt-0">
-              <NamePanel className={"block lg:hidden"} />
-              <Description className={"block lg:hidden"} />
+              <NamePanel
+                name={item.name}
+                category={item.category_name}
+                price={item.price}
+                sale_price={item.sale_price}
+                className={"block lg:hidden"}
+              />
+              <Description
+                description={item.description}
+                className={"block lg:hidden"}
+              />
               {/* <Size /> */}
             </div>
           </div>
@@ -151,25 +329,65 @@ function Item() {
         <div className="lg:flex-1/2 xl:flex-2/3 flex md:flex-col-reverse md:justify-center fixed bottom-0 w-screen p-5 lg:p-2 bg-white rounded-t-2xl lg:rounded-none lg:bg-backgroundcolor dark:bg-darkpalleteDark lg:dark:bg-backgroundcolorDark lg:static lg:bottom-auto lg:w-auto transition-colors duration-300">
           <div className="flex justify-between w-full items-center lg:flex-col-reverse lg:justify-center lg:gap-4 bg-white dark:bg-darkpalleteDark lg:bg-white dark:lg:bg-darkpalleteDark lg:px-3 lg:py-6 lg:rounded-3xl transition-colors duration-300">
             <div>
-              <button className="bg-primary dark:bg-primaryDark hover:bg-primaryDark dark:hover:bg-primary text-white text-2xl font-bold px-5 py-3 rounded-2xl transition-colors duration-300">
-                افزودن به سبد خرید
-              </button>
+              {orderCount === 0 ? (
+                <button
+                  onClick={handleAddToOrder}
+                  className="bg-primary dark:bg-primaryDark hover:bg-primaryDark dark:hover:bg-primary text-white text-2xl font-bold px-5 py-3 rounded-2xl transition-colors duration-300 animate-scale-up">
+                  افزودن به سبد خرید
+                </button>
+              ) : (
+                <div
+                  className={`flex items-center gap-6 ${
+                    removing ? "animate-scale-out" : "animate-scale-up"
+                  }`}>
+                  <button
+                    onClick={increaseCount}
+                    className="w-14 h-14 flex items-center justify-center bg-primary dark:bg-primaryDark rounded-full hover:bg-primaryDark dark:hover:bg-primary text-white transition-colors duration-300">
+                    <Plus className={"w-9 stroke-white"} />
+                  </button>
+
+                  <span className="w-12 text-center text-4xl font-bold dark:text-white inline-block">
+                    {orderCount}
+                  </span>
+
+                  <button
+                    onClick={decreaseCount}
+                    className="border-2 flex items-center justify-center border-primary dark:border-primaryDark hover:bg-primaryDark dark:hover:bg-primary hover:border-primaryDark dark:hover:border-primary text-white w-12 h-12 rounded-full transition-colors duration-300">
+                    <Minus
+                      className={
+                        "w-8 fill-black dark:fill-white hover:fill-white transition-colors duration-300"
+                      }
+                    />
+                  </button>
+                </div>
+              )}
             </div>
             <div className="w-20 lg:w-auto">
               <h1 className="text-xl font-bold lg:text-6xl dark:text-white transition-colors duration-300">
                 قیمت
               </h1>
-              <div className="flex justify-baseline items-center gap-1 lg:pt-5">
-                <h1 className="text-3xl font-extrabold lg:text-9xl dark:text-white transition-colors duration-300">
-                  85
+              <div className="flex justify-center items-end gap-1">
+                <h1 className="text-3xl font-bold dark:text-white transition-colors duration-300">
+                  {item.sale_price
+                    ? formatPrice(item.sale_price)
+                    : formatPrice(item.price)}
                 </h1>
-                <h3 className="font-normal lg:text-5xl dark:text-slowgray transition-colors duration-300">
+                <h3 className="dark:text-slowgray transition-colors duration-300">
                   تومان
                 </h3>
               </div>
             </div>
-            <Description className={"hidden lg:block w-full"} />
-            <NamePanel className={"hidden lg:block w-full"} />
+            <Description
+              description={item.description}
+              className={"hidden lg:block w-full"}
+            />
+            <NamePanel
+              name={item.name}
+              category={item.category_name}
+              price={item.price}
+              sale_price={item.sale_price}
+              className={"hidden lg:block w-full"}
+            />
           </div>
         </div>
       </div>
